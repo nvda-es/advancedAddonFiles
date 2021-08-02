c6a4adcf38c7468fb8c939889099a51e4d71ab16
Iván Novegil <ivan.novegil.cancelas@gmail.com>
2021-08-02 02:05:57 +0200

    Change Spanish by Spanish-speaking

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </main>
 <footer role="contentinfo">
-<p>This application has been developed by members of the spanish NVDA community, and is released under the GNU General Public License, version 2 or later. Contributions are very welcome in our <a href="https://github.com/nvda-es/advancedAddonFiles">GitHub repository</a>.</p>
+<p>This application has been developed by members of the Spanish-speaking NVDA community, and is released under the GNU General Public License, version 2 or later. Contributions are very welcome in our <a href="https://github.com/nvda-es/advancedAddonFiles">GitHub repository</a>.</p>
 </footer>
 </body>
 </html>
