ba93543ea058b61535f30e3917fd76630fb60039
José Manuel Delicado Alcolea <jmdaweb@hotmail.com>
2023-08-09 16:59:24 +0200

    Updated install.php to handle the hidden column

--- install.php
+++ install.php
@@ -6,8 +6,8 @@ if (file_exists($db_file)){
 	echo "<p>The database has been already created. If you want to reinstall this application, remove the database file first.</p>";
 }else{
 	$db=new SQLite3($db_file);
-	$db->exec("create table addons (id integer primary key, author text, name text, summary text, description text, url text, legacy integer)");
-	$db->exec("create table links (id integer, file text unique, version text, channel text, minimum text, lasttested text, link text, downloads integer, modified text)");
+	$db->exec("create table addons (id integer primary key, author text, name text, summary text, description text, url text, legacy integer default 0, hidden integer default 0)");
+	$db->exec("create table links (id integer, file text unique, version text, channel text, minimum text, lasttested text, link text, downloads integer, modified text, hidden integer default 0)");
 	$db->exec("create table users (id integer primary key, username text unique, fullname text, email text, password text, role integer)");
 	$db->exec("create table permissions (user integer, addon integer)");
 	$db->exec("insert into users (id, username, fullname, email, password, role) values (0, 'admin', 'Main administrator', 'admin@localhost', '".password_hash("admin", PASSWORD_DEFAULT)."', 2)");
